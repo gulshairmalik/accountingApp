443e431df83dbf090708ac30da0b8c295015d863
gulshairmalik <gulshairmalik@gmail.com>
2021-09-27 05:09:05 +0500

    Updating file upload path

--- routes/dso.js
+++ routes/dso.js
@@ -4,7 +4,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads/')
+    cb(null, __dirname + './../uploads')
   },
   filename: (req, file, cb) => {
     cb(null , file.originalname)
